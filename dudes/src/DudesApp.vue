63f346b198d6a302554c3f74d6a8c7aa01cacb74
nesqik21 <tomas.pudis@gmail.com>
2023-06-28 19:57:24 +0200

    comunication parent -> child

---
+++ dudes/src/DudesApp.vue
@@ -0,0 +1,40 @@
+<template>
+<div>
+    <ul>
+    <li v-for="(dude,index) in characters" :key="index">
+        <Dude :name="dude.name" :subtitle="dude.subtitle"/>
+    </li>
+</ul>
+
+<form @submit.prevent="characters.push(newDude )" class="add-new">
+    <input v-model="newDude" type="text" /> 
+</form>
+
+<p class="preview">
+    {{newDude }}
+</p>
+</div>
+</template>
+
+<script>
+import Dude from './components/Dude.vue';
+
+export default {
+    components: {
+        Dude,
+    },
+    data() {
+        return {
+            newDude:'',
+            characters: [
+                {name:'Homer Simpson',subtitle:'He is from The Simpsons series, where he is funny dude.'},
+                {name:'Cartman', subtitle:'He is from The Southpark series, he is small fat dude.'},
+                {name:'Rick and Morty',subtitle:'They are from the Rick and Morthy series, I dont know them'}]
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
---
+++ dudes/src/components/Dude.vue
@@ -0,0 +1,17 @@
+<template>
+        <article>
+            {{name}}
+            <span> {{subtitle}}</span>
+        </article> 
+
+</template>
+
+<script>
+export default {
+    props: ['subtitle','name']
+}
+</script>
+
+<style>
+
+</style>
